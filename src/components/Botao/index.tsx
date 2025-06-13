2cc3b53b7fcd027f159aaf6a35c9a9959aca41da
Raislan Ítalo <raislanitalo62@gmail.com>
2025-06-13 15:20:53 -0300

    Adicionado controle ao formulário

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -4,13 +4,15 @@ import style from './Botao.module.scss'
 // Define o tipo das props (incluindo children)
 interface BotaoProps {
     children?: React.ReactNode;
+    type?: "button" | "submit" | "reset" | undefined
 }
 
 // <{nome: tipo}> -> prop
 class Botao extends React.Component<BotaoProps>{
     render() {
+        const {type = "button"} = this.props;
         return(
-            <button className={style.botao}>
+            <button type={type} className={style.botao}>
                 {this.props.children}
             </button>
         )
--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import Botao from '../Botao';
 import style from './Formulario.module.scss'
+import { ITarefa } from '../../types/tarefa';
 
+class Formulario extends React.Component<{
+    setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+}> {
+    state = {
+        tarefa: "",
+        tempo: "00:00:00"
+    }
 
-class Formulario extends React.Component {
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+        evento.preventDefault();
+        this.props.setTarefas(tarefasAntigas => [...tarefasAntigas, {...this.state}]);
+    }
+
     render() {
         return(
-            <form className={style.novaTarefa}>
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor='tarefa'>
                         Adicione um novo estudo
                     </label>
                     <input type='text' name='tarefa'
                     id='tarefa'
+                    value = {this.state.tarefa}
+                    onChange={evento => {
+                        this.setState({...this.state, tarefa:evento.target.value})
+                    }}
                     placeholder='O que você quer estudar'
                     required/>
                 </div>
@@ -23,12 +39,18 @@ class Formulario extends React.Component {
                     <input type='time'
                     step='1'
                     name='tempo'
+                    value={this.state.tempo}
+                    onChange={evento => {
+                        this.setState({...this.state, tempo: evento.target.value })
+                    }}
                     id='tempo'
                     min='00:00:00'
                     max='01:30:00'
                     required/>
                 </div>
-                <Botao>Adicionar</Botao>
+                <Botao type="submit">
+                    Adicionar
+                </Botao>
             </form>
         )
     }
