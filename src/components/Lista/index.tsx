23b83c1b98ded9982ff609bdf0b466b10ada97af
Raislan Ítalo <raislanitalo62@gmail.com>
2025-06-13 11:24:04 -0300

    Adicionado estado para lista dinâmica

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './Lista.module.scss';
 import Item from './Item';
 
 // Function Component
 function Lista() {
-    const tarefas = [{
+    const [tarefas, setTarefas] = useState([{
         tarefa: 'React',
         tempo: '02:00:00'
     }, 
     {
         tarefa: 'JavaScript',
         tempo: '01:00:00'
-    }];
-
+    }]);
     return (
         <aside className={style.listaTarefas}>
-            <h2>Estudos do dia</h2>
+            <h2 onClick={() => {
+                console.log("H2 clicado: ", tarefas);
+                setTarefas([...tarefas, {tarefa: 'Estudar estado', tempo: '05:00:00'}])
+            }}>Estudos do dia</h2>
             { tarefas.map((item, index) => (
                 <div>
                     <ul>
